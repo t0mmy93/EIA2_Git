ba88c5f30b7e4d7c7b1ad723516e60bc8856f31b
tom <tom@DESKTOP-73F07AR.eduroam.hs-furtwangen.de>
2018-10-05 15:49:06 +0200

    aufgabe 0.1

---
+++ Aufgabe0.1/Aufgabe00.js
@@ -0,0 +1,13 @@
+var aufgabe0;
+(function (aufgabe0) {
+    var name = "";
+    function main() {
+        var k = prompt("Gib deinen Namen ein");
+        var node = document.getElementById("inhalt");
+        node.innerHTML += "Moin ";
+        node.innerHTML += name;
+        node.innerHTML += ", schoenen Tag.";
+        console.log("Moin " + name + ", schoenen Tag.");
+    }
+    document.addEventListener('DOMContentLoaded', main);
+})(aufgabe0 || (aufgabe0 = {}));
---
+++ Aufgabe0.1/Aufgabe00.ts
@@ -0,0 +1,17 @@
+
+namespace aufgabe0 {
+    var name : string ="";
+        
+    function main() {
+        var k = prompt("Gib deinen Namen ein");
+        var node : any = document.getElementById("inhalt");
+        node.innerHTML += "Moin ";
+        node.innerHTML += name;
+        node.innerHTML += ", schoenen Tag.";
+        console.log("Moin " + name + ", schoenen Tag.");
+       
+    }
+
+    
+    document.addEventListener('DOMContentLoaded',main);
+}
