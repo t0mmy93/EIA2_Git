49d2e5336a7b459daafeb9d6c2d42ff551e4e85e
t0mmy93 <43846955+t0mmy93@users.noreply.github.com>
2018-10-28 14:41:44 +0100

    Add files via upload

--- Aufgabe3/UNO_Online2.ts
+++ Aufgabe3/UNO_Online2.ts
@@ -9,53 +9,32 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 
 namespace unoonline4 {
 
-    let deck = ["r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "y0", "y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9", "g0", "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9", "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "r+2", "r+2", "g+2", "g+2", "y+2", "y+2", "b+2", "b+2", "rx", "rx", "gx", "gx", "yx", "yx", "bx", "bx", "r<=>", "r<=>", "g<=>", "g<=>", "y<=>", "y<=>", "b<=>", "b<=>", "n+4", "n+4", "n+4", "n+4", "n<c>", "n<c>", "n<c>", "n<c>"];
-    let r: string = "r";
-    let y: string = "y";
-    let g: string = "g";
-    let b: string = "b";
-    let n: string = "n";
-    // generiert eine zufällige Nummer
-    function random(x: number): number {
-        let randomNumber: number;
+    let deck: string[] = ["#ff00000", "#ff00001", "#ff00002", "#ff00003", "#ff00004", "#ff00005", "#ff00006", "#ff00007", "#ff00008", "#ff00009", "#ffff000", "#ffff001", "#ffff002", "#ffff003", "#ffff004", "#ffff005", "#ffff006", "#ffff007", "#ffff008", "#ffff009", "#00ff000", "#00ff001", "#00ff002", "#00ff003", "#00ff004", "#00ff005", "#00ff006", "#00ff007", "#00ff008", "#00ff009", "#0000ff0", "#0000ff1", "#0000ff2", "#0000ff3", "#0000ff4", "#0000ff5", "#0000ff6", "#0000ff7", "#0000ff8", "#0000ff9", "#ff00001", "#ff00002", "#ff00003", "#ff00004", "#ff00005", "#ff00006", "#ff00007", "#ff00008", "#ff00009", "#ffff001", "#ffff002", "#ffff003", "#ffff004", "#ffff005", "#ffff006", "#ffff007", "#ffff008", "#ffff009", "#00ff001", "#00ff002", "#00ff003", "#00ff004", "#00ff005", "#00ff006", "#00ff007", "#00ff008", "#00ff009", "#0000ff1", "#0000ff2", "#0000ff3", "#0000ff4", "#0000ff5", "#0000ff6", "#0000ff7", "#0000ff8", "#0000ff9", "#ff0000+2", "#ff0000+2", "#00ff00+2", "#00ff00+2", "#ffff00+2", "#ffff00+2", "#0000ff+2", "#0000ff+2", "#ff0000x", "#ff0000x", "#00ff00x", "#00ff00x", "#ffff00x", "#ffff00x", "#0000ffx", "#0000ffx", "#ff0000<=>", "#ff0000<=>", "#00ff00<=>", "#00ff00<=>", "#ffff00<=>", "#ffff00<=>", "#0000ff<=>", "#0000ff<=>", "#585858+4", "#585858+4", "#585858+4", "#585858+4", "#585858<c>", "#585858<c>", "#585858<c>", "#585858<c>"];
+
+    function random(x: number): number {                                    // generiert eine zufällige Nummer
         return Math.floor(Math.random() * Math.floor(x));
     }
-    // plaziert ein Span-Element 
-    function placeSpan(color: string, name: string, _q: number): void {   // 1.Farbe 2.Name 3.randomNumber
-        r = "#ff0000";
-        y = "#ffff00";
-        g = "#00ff00";
-        b = "#0000ff";
-        n = "#000000";
+    function placeSpan(color: string, name: string, _q: number): void {     // plaziert ein Span-Element {1.Farbe 2.Name 3.randomNumber}
         let span: HTMLSpanElement = document.createElement("span");
         document.body.appendChild(span);
         span.setAttribute("id", "a" + _q);
         document.getElementById("a" + _q).innerHTML += name;
         let s: CSSStyleDeclaration = span.style;
         s.backgroundColor = color;
-        //  console.log(name3);
     }
-    // main function
     function main(): void {
-
         let j: number;
         let i: string = prompt("Wie viele Karten pro Spieler?");
         j = Number(i);
-
         for (let d: number = 0; j > d; d++) {
             let l: number = random(deck.length);
-            
             let str: string = deck[l];
-            
-            let color: string = str.substring(0, 1);
-            console.log(str.substring(0, 1));
-            
-            let name: string = str.substring(1, 6);
-            console.log(str.substring(1, 6));
-
+            let color: string = str.substring(0, 7);                        // Farbcode wird aus dem Array gezogen
+            console.log(color);
+            let name: string = str.substring(7, 10);                        // Name wird aus dem Array gezogen
+            console.log(name);
             placeSpan(color, name, l);
-
-            let remove = deck.splice(l - 1, 1);
+            let remove = deck.splice(l - 1, 1);                             // Element wird aus dem Array gelöscht
             let lenght: number = deck.length;
             console.log(lenght);
             continue;
