5e9785f804934eb8ea02a8fedbbaa14df58151c9
tom <tom@DESKTOP-73F07AR>
2018-11-08 16:09:20 +0100

    lö

--- Aufgabe2/HTML/UNO_Online2.ts
+++ Aufgabe2/HTML/UNO_Online2.ts
@@ -6,51 +6,50 @@ Datum: (21.10.2018)
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-namespace unoonline4 {
+namespace UnoOnline4 {
+    document.addEventListener("DOMContentLoaded", main);
 
     let cardDeck: string[] = [
-        "#ff00000", "#ff00001", "#ff00002", "#ff00003", "#ff00004", "#ff00005", "#ff00006", "#ff00007", "#ff00008", "#ff00009",
-        "#00ff000", "#00ff001", "#00ff002", "#00ff003", "#00ff004", "#00ff005", "#00ff006", "#00ff007", "#00ff008", "#00ff009",
-        "#ffff000", "#ffff001", "#ffff002", "#ffff003", "#ffff004", "#ffff005", "#ffff006", "#ffff007", "#ffff008", "#ffff009",
-        "#0000ff0", "#0000ff1", "#0000ff2", "#0000ff3", "#0000ff4", "#0000ff5", "#0000ff6", "#0000ff7", "#0000ff8", "#0000ff9",
-        "#ff00001", "#ff00002", "#ff00003", "#ff00004", "#ff00005", "#ff00006", "#ff00007", "#ff00008", "#ff00009",
-        "#00ff001", "#00ff002", "#00ff003", "#00ff004", "#00ff005", "#00ff006", "#00ff007", "#00ff008", "#00ff009",
-        "#ffff001", "#ffff002", "#ffff003", "#ffff004", "#ffff005", "#ffff006", "#ffff007", "#ffff008", "#ffff009",
-        "#0000ff1", "#0000ff2", "#0000ff3", "#0000ff4", "#0000ff5", "#0000ff6", "#0000ff7", "#0000ff8", "#0000ff9",
-        "#ff0000+2", "#ff0000+2", "#00ff00+2", "#00ff00+2", "#ffff00+2", "#ffff00+2", "#0000ff+2", "#0000ff+2",
-        "#ff0000x", "#ff0000x", "#00ff00x", "#00ff00x", "#ffff00x", "#ffff00x", "#0000ffx", "#0000ffx",
-        "#ff0000chngDrctn", "#ff0000chngDrctn", "#00ff00chngDrctn", "#00ff00chngDrctn", "#ffff00chngDrctn", "#ffff00chngDrctn", "#0000ffchngDrctn", "#0000ffchngDrctn",
-        "#585858+4", "#585858+4", "#585858+4", "#585858+4", "#585858chngClr", "#585858chngClr", "#585858chngClr", "#585858chngClr"];
+        "#ff00000", "#ff00001", "#ff00002", "#ff00003", "#ff00004", "#ff00005", "#ff00006", "#ff00007", "#ff00008", "#ff00009",                                         // 0-9
+        "#00ff000", "#00ff001", "#00ff002", "#00ff003", "#00ff004", "#00ff005", "#00ff006", "#00ff007", "#00ff008", "#00ff009",                                         // 0-9
+        "#ffff000", "#ffff001", "#ffff002", "#ffff003", "#ffff004", "#ffff005", "#ffff006", "#ffff007", "#ffff008", "#ffff009",                                         // 0-9
+        "#0000ff0", "#0000ff1", "#0000ff2", "#0000ff3", "#0000ff4", "#0000ff5", "#0000ff6", "#0000ff7", "#0000ff8", "#0000ff9",                                         // 0-9
+        "#ff00001", "#ff00002", "#ff00003", "#ff00004", "#ff00005", "#ff00006", "#ff00007", "#ff00008", "#ff00009",                                                     // 1-9
+        "#00ff001", "#00ff002", "#00ff003", "#00ff004", "#00ff005", "#00ff006", "#00ff007", "#00ff008", "#00ff009",                                                     // 1-9
+        "#ffff001", "#ffff002", "#ffff003", "#ffff004", "#ffff005", "#ffff006", "#ffff007", "#ffff008", "#ffff009",                                                     // 1-9
+        "#0000ff1", "#0000ff2", "#0000ff3", "#0000ff4", "#0000ff5", "#0000ff6", "#0000ff7", "#0000ff8", "#0000ff9",                                                     // 1-9
+        "#ff0000+2", "#ff0000+2", "#00ff00+2", "#00ff00+2", "#ffff00+2", "#ffff00+2", "#0000ff+2", "#0000ff+2",                                                         // +2
+        "#ff0000x", "#ff0000x", "#00ff00x", "#00ff00x", "#ffff00x", "#ffff00x", "#0000ffx", "#0000ffx",                                                                 // Aussetzen
+        "#ff0000chngDrctn", "#ff0000chngDrctn", "#00ff00chngDrctn", "#00ff00chngDrctn", "#ffff00chngDrctn", "#ffff00chngDrctn", "#0000ffchngDrctn", "#0000ffchngDrctn", // Richtungswechsel
+        "#585858+4", "#585858+4", "#585858+4", "#585858+4",                                                                                                             // +4
+        "#585858chngClr", "#585858chngClr", "#585858chngClr", "#585858chngClr"];                                                                                       // change Color
     //      console.log(cardDeck.length);
-    function random(x: number): number {
+    function randomNumber(x: number): number {
         return Math.floor(Math.random() * Math.floor(x));
     }
-    function placeSpan(cardColor: string, cardName: string, _q: number): void {
+    function placeSpan(_cardColor: string, _cardName: string, _q: number): void {
         let span: HTMLSpanElement = document.createElement("span");
         document.body.appendChild(span);
         span.setAttribute("id", "a" + _q);
-        span.innerHTML = cardName;
+        span.innerHTML = _cardName;
         let s: CSSStyleDeclaration = span.style;
-        s.backgroundColor = cardColor;
+        s.backgroundColor = _cardColor;
     }
-    
+
     function main(): void {
-        let j: number;
-        let i: string = prompt("Wie viele Karten pro Spieler?");
-        j = Number(i);
-        for (let d: number = 0; j > d; d++) {
-            let l: number = random(cardDeck.length);
-            let str: string = cardDeck[l];
+        let cardsPulled: number;
+        let cardsToPull: string = prompt("Wie viele Karten pro Spieler?");
+        cardsPulled = Number(cardsToPull);
+        for (let i: number = 0; cardsPulled > i; i++) {
+            let randomCard: number = randomNumber(cardDeck.length);
+            let str: string = cardDeck[randomCard];
             let cardColor: string = str.substring(0, 7);                        // Farbcode wird aus dem Array gezogen
             console.log(cardColor);
             let cardName: string = str.substring(7, 19);                        // cardName wird aus dem Array gezogen
             console.log(cardName);
-            placeSpan(cardColor, cardName, l);
-            cardDeck.splice(l - 1, 1);                             // Element wird aus dem Array gelöscht
-            let lenght: number = cardDeck.length;
-            console.log(lenght);
-            continue;
+            placeSpan(cardColor, cardName, randomCard);
+            cardDeck.splice(randomCard - 1, 1);                             // Element wird aus dem Array gelöscht
+            console.log(cardDeck.length);
         }
     }
-    document.addEventListener("DOMContentLoaded", main);
 }
