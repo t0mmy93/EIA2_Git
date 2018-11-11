ba9cb2247f500d6ab77101a2830c3d30c9176d51
tom <tom@DESKTOP-73F07AR.eduroam.hs-furtwangen.de>
2018-11-11 17:29:49 +0100

    Aufgabe3 nicht fertig

--- Aufgabe_3/UNO_Online3.js
+++ Aufgabe_3/UNO_Online3.js
@@ -10,7 +10,18 @@ var UnoOnline_A3;
 (function (UnoOnline_A3) {
     document.addEventListener("DOMContentLoaded", main);
     //  document.addEventListener("DOMContentLoaded", input);
-    //  document.addEventListener("keydown", pressSpacebar);
+    // document.addEventListener("keydown", pressSpacebar);
+    /*
+          function input(): void {
+      
+              let button: HTMLElement = document.getElementById("button");
+              button.addEventListener("click", sort);
+      
+              let pull: HTMLElement = document.getElementById("Nachziehstapel");
+              pull.addEventListener("click", getRandomCard);
+      
+          }
+      */
     let cardDeck = [
         "#ff00000", "#ff00001", "#ff00002", "#ff00003", "#ff00004", "#ff00005", "#ff00006", "#ff00007", "#ff00008", "#ff00009",
         "#00ff000", "#00ff001", "#00ff002", "#00ff003", "#00ff004", "#00ff005", "#00ff006", "#00ff007", "#00ff008", "#00ff009",
@@ -27,15 +38,20 @@ var UnoOnline_A3;
         "#585858chngClr", "#585858chngClr", "#585858chngClr", "#585858chngClr"]; // change Color
     //      console.log(cardDeck.length);
     let handCards = [];
+    console.log(handCards);
     function randomNumber(x) {
         return Math.floor(Math.random() * Math.floor(x));
     }
-    function placeDiv(_cardColor, _cardName, _q) {
-        let span = document.createElement("span");
-        document.body.appendChild(span);
-        span.setAttribute("id", "a" + _q);
-        span.innerHTML = _cardName;
-        let s = span.style;
+    function placeDiv(_cardColor, _cardName, _divNumber) {
+        let div = document.createElement("div");
+        let hand = document.getElementById("Hand");
+        hand.appendChild(div);
+        //      document.body.appendChild(div);
+        let id = _divNumber.toString();
+        div.setAttribute("id", id);
+        div.classList.add("Handcards");
+        div.innerHTML = _cardName;
+        let s = div.style;
         s.backgroundColor = _cardColor;
     }
     //    function addCardsToArray(_cardColor: string, _cardValue: string): void {
@@ -50,14 +66,62 @@ var UnoOnline_A3;
             let randomCard = randomNumber(cardDeck.length);
             let str = cardDeck[randomCard];
             let cardColor = str.substring(0, 7); // Farbcode wird aus dem Array gezogen
-            console.log(cardColor);
+            //            console.log(cardColor);
             let cardName = str.substring(7, 19); // cardName wird aus dem Array gezogen
-            console.log(cardName);
-            placeDiv(cardColor, cardName, randomCard);
+            //            console.log(cardName);
+            placeDiv(cardColor, cardName, i);
             cardDeck.splice(randomCard - 1, 1); // Element wird aus dem Array gelöscht
             console.log(cardDeck.length);
             handCards.push(str);
+            handCards.sort();
+        }
+    }
+    function displayHand() {
+        document.getElementById("");
+        for (let j = 0; j < handCards.length; j++) {
+            let div = document.createElement("div");
+            document.getElementById("Hand").appendChild(div);
+            let str = handCards[j];
+            let cardColor = str.substring(0, 7); // Farbcode wird aus dem Array gezogen
+            console.log(cardColor);
+            let cardName = str.substring(7, 19); // cardName wird aus dem Array gezogen
+            console.log(cardName);
+            div.innerHTML = cardName;
+            let id = j.toString();
+            div.setAttribute("id", id);
+            div.classList.add("Handcards");
+            let s = div.style;
+            s.backgroundColor = cardColor;
+        }
+        /*
+                function dropCard(_event: MouseEvent): void {
+                    document.getElementById("Stack").innerHTML = "",
+                        let clickedCard: HTMLElement = <HTMLElement>_event.target;
+                    let index: number = parseInt(clickedCard.id);
+                    placeToKeep.push(handCards[index]);
+                    let div: HTMLElement = document.createElement("div");
+                    document.getElementById("Stack").appendChild(div);
+                    let str: string = handCards[index];
+                    let cardColor: string = str.substring(0, 7);                        // Farbcode wird aus dem Array gezogen
+                    console.log(cardColor);
+                    let cardName: string = str.substring(7, 19);                        // cardName wird aus dem Array gezogen
+                    console.log(cardName);
+                    div.innerHTML = cardName;
+                    div.classList.add(cardColor);
+                    handCards.splice(index, 1);
+                    displayHand();
+                }
+        */
+        function sort() {
+            document.getElementById("Hand").innerHTML = "";
+            handCards.sort();
             console.log(handCards);
+            //displayHand();
+        }
+        function getRandomCard(_x) {
+            let randomCard = randomNumber(cardDeck.length);
+            handCards.push(cardDeck[randomCard]);
+            cardDeck.splice(randomCard, 1);
         }
     }
 })(UnoOnline_A3 || (UnoOnline_A3 = {}));
--- Aufgabe_3/UNO_Online3.ts
+++ Aufgabe_3/UNO_Online3.ts
@@ -8,8 +8,23 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 */
 namespace UnoOnline_A3 {
     document.addEventListener("DOMContentLoaded", main);
+
     //  document.addEventListener("DOMContentLoaded", input);
-    //  document.addEventListener("keydown", pressSpacebar);
+    // document.addEventListener("keydown", pressSpacebar);
+/*
+      function input(): void {
+  
+          let button: HTMLElement = document.getElementById("button");
+          button.addEventListener("click", sort);
+  
+          let pull: HTMLElement = document.getElementById("Nachziehstapel");
+          pull.addEventListener("click", getRandomCard);
+  
+      }
+  */
+
+
+
 
 
     let cardDeck: string[] = [
@@ -28,20 +43,23 @@ namespace UnoOnline_A3 {
         "#585858chngClr", "#585858chngClr", "#585858chngClr", "#585858chngClr"];                                                                                       // change Color
     //      console.log(cardDeck.length);
 
-    let handCards: string[] = [ ];
-
+    let handCards: string[] = [];
+    console.log(handCards);
     function randomNumber(x: number): number {
         return Math.floor(Math.random() * Math.floor(x));
     }
-    function placeDiv(_cardColor: string, _cardName: string, _q: number): void {
-        let span: HTMLSpanElement = document.createElement("span");
-        document.body.appendChild(span);
-        span.setAttribute("id", "a" + _q);
-        span.innerHTML = _cardName;
-        let s: CSSStyleDeclaration = span.style;
+    function placeDiv(_cardColor: string, _cardName: string, _divNumber: number): void {
+        let div: HTMLDivElement = document.createElement("div");
+        let hand: HTMLElement = document.getElementById("Hand");
+        hand.appendChild(div);
+        //      document.body.appendChild(div);
+        let id: string = _divNumber.toString();
+        div.setAttribute("id", id);
+        div.classList.add("Handcards");
+        div.innerHTML = _cardName;
+        let s: CSSStyleDeclaration = div.style;
         s.backgroundColor = _cardColor;
     }
-
     //    function addCardsToArray(_cardColor: string, _cardValue: string): void {
     //        handCards.push(_cardColor, _cardValue);
     //    document.getElementById("demo").innerHTML = fruits;
@@ -57,19 +75,82 @@ namespace UnoOnline_A3 {
 
             let str: string = cardDeck[randomCard];
             let cardColor: string = str.substring(0, 7);                        // Farbcode wird aus dem Array gezogen
-            console.log(cardColor);
+            //            console.log(cardColor);
             let cardName: string = str.substring(7, 19);                        // cardName wird aus dem Array gezogen
-            console.log(cardName);
+            //            console.log(cardName);
 
-            placeDiv(cardColor, cardName, randomCard);
+            placeDiv(cardColor, cardName, i);
             cardDeck.splice(randomCard - 1, 1);                             // Element wird aus dem Array gelöscht
             console.log(cardDeck.length);
             handCards.push(str);
-            console.log(handCards);
-        }
-
-
-
-
+      //      handCards.sort();
+
         }
     }
+    function displayHand(): void {
+        document.getElementById("");
+
+        for (let j: number = 0; j < handCards.length; j++) {
+            let div: HTMLElement = document.createElement("div");
+            document.getElementById("Hand").appendChild(div);
+            let str: string = handCards[j];
+            let cardColor: string = str.substring(0, 7);                        // Farbcode wird aus dem Array gezogen
+            console.log(cardColor);
+            let cardName: string = str.substring(7, 19);                        // cardName wird aus dem Array gezogen
+            console.log(cardName);
+            div.innerHTML = cardName;
+            let id: string = j.toString();
+            div.setAttribute("id", id);
+            div.classList.add("Handcards");
+            let s: CSSStyleDeclaration = div.style;
+            s.backgroundColor = cardColor;
+
+            //   div.addEventListener("click", dropCard);
+        }
+        /*
+                function dropCard(_event: MouseEvent): void {
+                    document.getElementById("Stack").innerHTML = "",
+                        let clickedCard: HTMLElement = <HTMLElement>_event.target;
+                    let index: number = parseInt(clickedCard.id);
+                    placeToKeep.push(handCards[index]);
+                    let div: HTMLElement = document.createElement("div");
+                    document.getElementById("Stack").appendChild(div);
+                    let str: string = handCards[index];
+                    let cardColor: string = str.substring(0, 7);                        // Farbcode wird aus dem Array gezogen
+                    console.log(cardColor);
+                    let cardName: string = str.substring(7, 19);                        // cardName wird aus dem Array gezogen
+                    console.log(cardName);
+                    div.innerHTML = cardName;
+                    div.classList.add(cardColor);
+                    handCards.splice(index, 1);
+                    displayHand();
+                }
+        */
+
+        function sort(): void {
+
+            document.getElementById("Hand").innerHTML = "";
+            handCards.sort();
+            console.log(handCards);
+            //displayHand();
+
+
+        }
+
+
+        function getRandomCard(_x: number): void {
+            let randomCard: number = randomNumber(cardDeck.length);
+            handCards.push(cardDeck[randomCard]);
+            cardDeck.splice(randomCard, 1);
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
