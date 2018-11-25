b09e2ac34b52cac959543f718bf3e9fa30eaaabc
t0mmy93 <43846955+t0mmy93@users.noreply.github.com>
2018-11-25 17:52:18 +0100

    Add files via upload

--- Aufgabe_5/algorythm.js
+++ Aufgabe_5/algorythm.js
@@ -1,5 +1,6 @@
 var Aufgabe_5;
 (function (Aufgabe_5) {
+    //   console.log(data["Dekoration"][0].color[0]);
     document.addEventListener("DOMContentLoaded", init);
     function init(_event) {
         console.log(Aufgabe_5.data);
@@ -19,7 +20,7 @@ var Aufgabe_5;
         for (let categories in _productCategories) {
             let value = _productCategories[categories];
             placeDiv();
-            console.group("1" + categories);
+            console.group("Kategorie" + categories);
             console.dir(value.length);
             console.groupEnd();
             for (let i = 0; i < 2; i++) {
@@ -31,33 +32,124 @@ var Aufgabe_5;
                 div1.innerHTML = ("Baumarten");
             }
             for (let i = 0; i < value.length; i++)
-                displayProducts(value[i], categories);
+                displayProducts(value[i], categories, value.length);
         }
     }
-    function displayProducts(_products, _categoryName) {
+    function displayProducts(_products, _categoryName, _value) {
+        //  console.log("gg", _value );
         let fieldset = document.createElement("fieldset");
-        let input = document.createElement("input");
         let legend = document.createElement("legend");
         let label = document.createElement("label");
-        label.setAttribute = ("id", _products.name.toString);
-        fieldset.setAttribute("price", _products.price.toString());
-        fieldset.setAttribute("id", _products.name.toString());
-        fieldset.appendChild(legend);
-        if (_categoryName == "Dekoration") {
-            document.getElementById("div[0]").appendChild(fieldset);
-            //  document.getElementById("div[0]").innerHTML += (_categoryName);
-            for (let i = 0; i < _categoryName.length; i++)
-                label.innerText = _products.color[i];
-            input.setAttribute("type", _products.inputType);
+        let input = document.createElement("input");
+        /*
+                function getColors(_dekorationIndex: number): void {
+                    for (let i: number = 0; i < data["Dekoration"][_dekorationIndex].color[i].length; i++) {
+                        let colorName: string = data["Dekoration"][_dekorationIndex].color[i];
+                        console.log("color " + data["Dekoration"][_dekorationIndex].color[i].length);
+                        label.innerText = colorName;
+                    }
+                    
+          }
+             
+           function getColors2(_arrayNumber: number): void {
+        for (let j: number = 0; j < data["Dekoration"][_arrayNumber].name.length; j++) { // 2
+            label.innerText = data["Dekoration"][_arrayNumber].color[1];
+            label.appendChild(document.createElement("br"));
+
+            label.setAttribute("type", _products.inputType);
+            label.setAttribute("id", _products.color.toString());
+            document.getElementById(data["Dekoration"][_arrayNumber].name + " legend").appendChild(label);
         }
-        else
-            document.getElementById("div[1]").appendChild(fieldset);
-        legend.innerText = _products.name.toString();
     }
+        
+        */
+        function getColors2(_arrayIndex) {
+            //    console.log(_arrayIndex + "ARAAYNUMMER");
+            for (let i = 0; i < Aufgabe_5.data["Dekoration"][_arrayIndex].color.length; i++) {
+                console.log(Aufgabe_5.data["Dekoration"][_arrayIndex].color[0] + " FARBE");
+                label.innerText = Aufgabe_5.data["Dekoration"][_arrayIndex].color[i];
+                label.appendChild(document.createElement("br"));
+                label.setAttribute("type", _products.inputType);
+                label.setAttribute("id", _products.color.toString());
+                document.getElementById(Aufgabe_5.data["Dekoration"][_arrayIndex].name + " legend").appendChild(label);
+            }
+        }
+        if (_categoryName == "Dekoration") {
+            console.log(_products.name, "cecec");
+            input.classList.add("class", _products.name);
+            input.classList.add("formelements");
+            input.setAttribute("type", _products.inputType);
+            fieldset.setAttribute("id", _products.name + " fieldset");
+            document.getElementById("div[0]").appendChild(fieldset);
+            legend.innerText = _products.name.toString();
+            legend.appendChild(document.createElement("br"));
+            legend.setAttribute("id", _products.name + " legend");
+            document.getElementById(_products.name + " fieldset").appendChild(legend);
+            input.setAttribute("id", _products.name + " input");
+            document.getElementById(_products.name + " legend").appendChild(input);
+            for (let i = 0; i < Aufgabe_5.data["Dekoration"].length; i++) {
+                //      console.log(data["Dekoration"].length + " Dekoration laenge");
+                getColors2(0);
+            }
+        }
+        else {
+            input.classList.add("class", _products.name);
+            input.classList.add("formelements");
+            input.setAttribute("type", _products.inputType);
+            fieldset.setAttribute("id", _products.name);
+            document.getElementById("div[0]").appendChild(fieldset);
+            legend.innerText = _products.name.toString();
+            document.getElementById(_products.name).appendChild(legend);
+            document.getElementById(_products.name).appendChild(input);
+            fieldset.innerText = _products.name.toString();
+            for (let i = 0; i < _value; i++) {
+                let color = _products.color[i];
+                label.innerText = color;
+                label.setAttribute("type", _products.inputType);
+                document.getElementById(_products.name).appendChild(label);
+                label.appendChild(document.createElement("br"));
+            }
+        }
+        //   }
+    }
+    //        else
+    //
+    //    document.getElementById("div[1]").appendChild(fieldset);
+    //    legend.innerText = _products.name.toString();
+    //    document.getElementsByClassName("legend");
+    //    legend.appendChild(label);
+    //}
     function handleClick(_event) {
         let target = _event.target;
         if (target.tagName != "fieldset")
             alert(target.getAttribute("result"));
     }
 })(Aufgabe_5 || (Aufgabe_5 = {}));
+/*
+Cut fucntion noch ab�ndern
+
+  function getIndex(_itemName: string, _arrayWarenkorb: string[], _arrayToSearch: string[]): void {
+        let index: number = _arrayWarenkorb.indexOf(_itemName);      // z.b 2
+        // console.log(index);
+        if (index == -1) {
+            console.log("Produkt ist nicht vorhanden.");
+        } else {
+            for (let i: number = 0; i < _arrayToSearch.length; i++) {
+                let itemToCut: string = _arrayToSearch[i];
+                let cutIndex: number = _arrayWarenkorb.indexOf(itemToCut);
+                if (cutIndex != -1) {
+                    _arrayWarenkorb.splice(cutIndex, 1);
+                } else {
+                    console.log("das Produkt ist nicht im Warenkorb.");
+                }
+            }
+        }
+        shoppingCartName.push(_itemName);
+        //    console.log(shoppingCartName);
+    }
+*/
+//function displayCart(_cartInfo: string):void {
+//
+//
+//}
 //# sourceMappingURL=algorythm.js.map
--- Aufgabe_5/data.ts
+++ Aufgabe_5/data.ts
@@ -1,5 +1,5 @@
 namespace Aufgabe_5 {
-
+    export let cart: string[] = [];
     export interface Products {
         name: string;
         price: number;
@@ -15,21 +15,21 @@ namespace Aufgabe_5 {
     export let data: ProductCategories = {
 
         "Dekoration": [
-            { name: "Kerzen", price: 0.99, color: ["rot", "blau", "gold", "Supreme", "Gucci"], inputType: "checkbox", stepper: true},
-            { name: "Baum-Kugeln", price: 1.99, color: ["rot", "blau", "gold", "Supreme", "Gucci"], inputType: "checkbox", stepper: true},
-            { name: "Lichterketten", price: 1.99, color: ["3m", "6m", "10m", "Prada", "VVS"], inputType: "checkbox", stepper: true},
-            { name: "Engel", price: 14.99, color: ["Schlank", "Fett"], inputType: "radio", stepper: false},
-            { name: "Stern", price: 14.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false},
-            { name: "Supreme-Logo", price: 154.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false},
-            { name: "Gucii-Logo", price: 229.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false},
-            { name: "VVS-Logo", price: 449.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false}
+            { name: "Kerzen", price: 0.99, color: ["rot", "blau", "gold", "Supreme", "Gucci"], inputType: "checkbox", stepper: true },
+            { name: "Baum-Kugeln", price: 1.99, color: ["rot", "blau", "gold", "Supreme", "Gucci"], inputType: "checkbox", stepper: true },
+            { name: "Lichterketten", price: 1.99, color: ["3m", "6m", "10m", "Prada", "VVS"], inputType: "checkbox", stepper: true },
+            { name: "Engel", price: 14.99, color: ["Schlank", "Fett"], inputType: "radio", stepper: false },
+            { name: "Stern", price: 14.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false },
+            { name: "Supreme-Logo", price: 154.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false },
+            { name: "Gucii-Logo", price: 229.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false },
+            { name: "VVS-Logo", price: 449.99, color: ["Einheitsgroesse"], inputType: "radio", stepper: false }
 
         ],
         "Baumarten": [
-            { name: "Nordmantanne", price: 34.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false},
-            { name: "Fichte", price: 24.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false},
-            { name: "Rotfichte", price: 29.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false},
-            { name: "VVS", price: 349.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false}
+            { name: "Nordmantanne", price: 34.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false },
+            { name: "Fichte", price: 24.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false },
+            { name: "Rotfichte", price: 29.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false },
+            { name: "VVS", price: 349.99, color: ["klein", "mittel", "gross"], inputType: "radio", stepper: false }
         ]
     };
 
