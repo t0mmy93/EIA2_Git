7be783fb3d2aea7d81175a81d5243d4bee2009c6
t0mmy93 <43846955+t0mmy93@users.noreply.github.com>
2018-12-05 23:29:26 +0100

    Update Server.ts

--- Aufgabe_6/Server/Server.ts
+++ Aufgabe_6/Server/Server.ts
@@ -1,5 +1,5 @@
-import * as Http from "http";
 import * as Url from "url";
+import * as Http from "http";
 
 namespace L06_SendData {
     interface product {
@@ -9,45 +9,26 @@ namespace L06_SendData {
     let port: number = process.env.PORT;
     if (port == undefined)
         port = 8100;
-
     let server: Http.Server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
-
     function handleListen(): void {
         console.log("Listening");
     }
-
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("_request url");
-
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-
-
         console.log(_request.url);
-
-        //Aufgabe 7
+        // NEU
         let url: product = Url.parse(_request.url, true).query;
         console.log(url);
-
         for (let key in url) {
             console.log(url[key]);
             console.log(key);
-
             _response.write(key + " = " + url[key] + "<br>");
         }
-
-
-
-
-
-
-
-
-
         _response.end();
     }
-
 }
